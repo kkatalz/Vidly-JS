da856cf69a254381a99df3a725cf108eb28216ad
kkatalz <zlata.kiev2004@gmail.com>
2025-06-02 17:37:36 +0300

    Add endpoints for customers.

--- routes/customers.js
+++ routes/customers.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const customerSchema = mongoose.Schema({
   isGold: Boolean,
-  name: String,
-  phone: String,
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
 });
 
 const Customer = mongoose.model("Customer", customerSchema);
@@ -26,6 +26,58 @@ async function createCustomer() {
   }
 }
 
-createCustomer();
+// createCustomer();
+
+router.get("/", async (req, res) => {
+  const customers = await Customer.find().sort("name");
+  res.send([customers]);
+});
+
+router.get("/:id", async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(400).send("Invalid customer ID.");
+
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer)
+    return res.status(404).send("No such customer found with given ID");
+  res.send(customer);
+});
+
+router.post("/", async (req, res) => {
+  let customer = new Customer(req.body);
+
+  try {
+    const result = await customer.save();
+    res.send(result);
+  } catch (ex) {
+    res.status(400).send(ex.message);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!customer)
+      return res
+        .status(404)
+        .send("The customer with the given ID was not found");
+    res.send(customer);
+  } catch (ex) {
+    res.status(400).send(ex.message);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndDelete(req.params.id);
+
+  if (!customer)
+    return res.status(404).send("The customer with the given ID was not found");
+
+  res.send(customer);
+});
 
 module.exports = router;
