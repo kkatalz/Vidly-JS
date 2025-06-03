c23acf73ce1b2fab97a6a39f1ac148f3e597dfeb
kkatalz <zlata.kiev2004@gmail.com>
2025-06-03 16:44:02 +0300

    Add a new route.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const home = require("./routes/home");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 
 mongoose
   .connect("mongodb://localhost:27017/")
@@ -15,6 +16,7 @@ app.use(express.json());
 // Use routers
 app.use("/", home);
 app.use("/vidly/api/genres", genres);
+app.use("/vidly/api/movies", movies);
 app.use("/vidly/api/customers", customers);
 
 const PORT = process.env.PORT || 3000;
