8a2bef8a14bac41325b2a65b61cea0b22c047331
kkatalz <zlata.kiev2004@gmail.com>
2025-06-04 12:42:32 +0300

    Add lodash usage.

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,8 @@
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
-
-// router.get("/", async (req, res) => {
-//   const users = User.find().sort("name");
-//   res.send([users]);
-// });
-
-// router.get("/:id", async (req, res) => {
-//   if (!mongoose.Types.ObjectId.isValid(req.params.id))
-//     return res.status(400).send("Invalid customer ID.");
-
-//   const user = await User.findById(req.params.id);
-
-//   if (!user) return res.status(404).send("No such user found with given ID");
-
-//   res.send(user);
-// });
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -26,15 +11,11 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
+    user = new User(_.pick(req.body, ["name", "email", "password"]));
     await user.save();
-    res.send(user);
+
+    res.send(_.pick(user, ["_id", "name", "email"]));
   } catch (ex) {
     res.status(400).send(ex.message);
   }
