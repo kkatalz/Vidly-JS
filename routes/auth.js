0c5a8a47e5098453de7b9fe375202b9868e395c5
kkatalz <zlata.kiev2004@gmail.com>
2025-06-04 14:34:35 +0300

    Configure json web token.

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -18,7 +20,8 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  res.send(token);
 });
 
 function validate(req) {
