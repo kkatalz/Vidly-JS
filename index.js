9a658aaac4505b1822405e8715a0dafb8c698651
kkatalz <zlata.kiev2004@gmail.com>
2025-06-06 13:21:41 +0300

    Extract routes to a different file.

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const winston = require("winston");
 require("winston-mongodb");
-const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const home = require("./routes/home");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
+require("./startup/routes")(app);
+
 
 // Catch uncaught exceptions
 /*
@@ -31,7 +25,7 @@ process.on("unhandledRejection", (ex) => {
 });
 */
 
-// TO AUTOMATE TWO EXCEPTIONs ABOUT  USE WINSTON
+// TO AUTOMATE TWO EXCEPTIONs ABOVE USE WINSTON
 winston.handleExceptions(
   new winston.transports.File({ filename: "uncaughtExceptions.log" })
 );
@@ -67,19 +61,6 @@ mongoose
   .connect("mongodb://localhost:27017/vidly")
   .then(() => console.log("Connected to MongoDb..."))
   .catch((err) => console.error("Could not connect to MongoDb", err));
-
-app.use(express.json());
-
-// Use routers
-app.use("/", home);
-app.use("/api/genres", genres);
-app.use("/api/movies", movies);
-app.use("/api/customers", customers);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
