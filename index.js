7ad21358e4f2c3aebb04931f2274a63765a747f9
kkatalz <zlata.kiev2004@gmail.com>
2025-06-09 13:50:48 +0300

    Extract module.

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ require("./startup/config")();
 require("./startup/validation")();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => winston.info(`Listening on port ${PORT}...`));
+const server = app.listen(PORT, () =>
+  winston.info(`Listening on port ${PORT}...`)
+);
+
+module.exports = server;
