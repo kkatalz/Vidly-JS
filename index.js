e46393b0d01f7b7d7f2c854a547884f5d7e0dcb0
kkatalz <zlata.kiev2004@gmail.com>
2025-06-14 11:34:57 +0300

    Add helmet.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () =>
